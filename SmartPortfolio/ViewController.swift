ae8c3e102182cea9dec89983a211ca5ac54debad
Kirill Ki <kirillkalapov@gmail.com>
2021-07-23 16:38:10 +0200

    Initial Commit

---
+++ SmartPortfolio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartPortfolio
+//
+//  Created by  Mr.Ki on 23.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
